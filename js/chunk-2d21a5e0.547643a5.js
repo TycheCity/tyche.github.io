a2b68d3b82f80de9e119dafc8f9212b07305741e
Eayon Wong <eayon@EayondeMacBook-Pro.local>
2019-02-17 02:28:31 +0800

    update commit

---
+++ js/chunk-2d21a5e0.547643a5.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a5e0"],{bae0:function(n,e,t){"use strict";t.r(e);var l=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n    Download Trust Wallet\n")])},a=[],u={},c=u,o=t("2877"),r=Object(o["a"])(c,l,a,!1,null,null,null);e["default"]=r.exports}}]);
